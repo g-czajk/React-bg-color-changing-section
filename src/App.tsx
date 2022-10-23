6986eb54c93bb91c28bfdc7c206e248c429e17d6
Grzegorz Czajkowski <czajkowski.grz@gmail.com>
2022-10-23 17:17:49 +0200

    devlop Cta section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import "./components/Cta/index.css"
+import Cta from "./components/Cta/index"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <section className="container scroll-through">
+                <h2>Scroll down</h2>
+            </section>
+            <Cta
+                text="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maiores deserunt voluptatibus, maxime repudiandae quibusdam et eos autem ratione voluptas."
+                backgroundFrom="#4338ca"
+                backgroundTo="#9333ea"
+            />
+            <section className="container scroll-through">
+                <h2>Scroll up</h2>
+            </section>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Cta/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef } from "react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+export interface Props {
+    text: string
+    description: string
+    backgroundFrom: string
+    backgroundTo: string
+}
+
+const Pinned: React.FC<Props> = ({ text, description, backgroundFrom, backgroundTo }) => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const sectionEl = useRef<HTMLElement>(null)
+    const buttonEl = useRef<HTMLButtonElement>(null)
+
+    // flag to make useEffect run once in React strict mode (dev) - otherwise gsap gets bugged
+    let ignore = false
+
+    useEffect(() => {
+        if (!ignore) {
+            gsap.set(sectionEl.current, {
+                backgroundColor: backgroundFrom,
+            })
+
+            gsap.to(sectionEl.current, {
+                backgroundColor: backgroundTo,
+                scrollTrigger: {
+                    trigger: sectionEl.current,
+                    scrub: true,
+                    start: "bottom bottom",
+                    end: "center center",
+                },
+            })
+
+            gsap.to([buttonEl.current], {
+                color: backgroundTo,
+                scrollTrigger: {
+                    trigger: sectionEl.current,
+                    scrub: true,
+                    start: "bottom bottom",
+                    end: "center center",
+                },
+            })
+        }
+
+        return () => {
+            ignore = true
+        }
+    }, [])
+
+    return (
+        <section ref={sectionEl} className="container cta">
+            <div className="content">
+                <h2>{text}</h2>
+                <p>{description}</p>
+                <button ref={buttonEl}>Learn more</button>
+            </div>
+        </section>
+    )
+}
+
+export default Pinned
